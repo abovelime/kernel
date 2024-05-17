e652e6e94702952f8c2085b131329b1f00f0bbe0
Liam B <143614295+abovelime@users.noreply.github.com>
2024-05-17 22:29:10 +1000

    it works! can use help installos at the moment. also deleted installOSBoot.js

--- scripts/installOS/command.js
+++ scripts/installOS/command.js
@@ -1,16 +1,17 @@
 let answerCount = 1;
+let response;
 
 const kernel = document.getElementById("kernel");
 const active = document.getElementById("active");
 
-var command = document.getElementById("command");
+let command = document.getElementById("command");
 command.addEventListener("keydown", function (e) {
    if (e.key === "Enter") {
-       whatCommand()
+       printCommand()
    }
 });
 
-function whatCommand() {
+function printCommand() {
     // make new div
     const answerDiv1 = document.createElement('div');
     answerDiv1.setAttribute('id', 'answer'+answerCount);
@@ -31,7 +32,14 @@ function whatCommand() {
 
     // COMMAND CHECK HERE
 
+    commandCheck()
 
+    const responseP = document.createElement('p');
+    const responseNode = document.createTextNode(response);
+    responseP.appendChild(responseNode);
+    responseP.setAttribute('id', 'response'+answerCount);
+
+    answerDiv1.appendChild(responseP);
 
     // Clear Input
 
@@ -39,3 +47,21 @@ function whatCommand() {
 
 }
 
+function commandCheck() {
+
+    // it works?
+    let args = command.value.split(" ");
+    console.log(args[0]);
+
+    // checks for the help arg
+    if (args[0] === "help") {
+        if (args[1] === "installos") {
+            response = "Hello! Welcome to the AboveLime Kernel! This is in the installOS sector where you get to install your operating system! This is built on my bad javascript code and is really unreliable. Project is on GitHub. Use the command, 'installos' to start the installer! Thanks for tuning in. AboveLime - Liam";
+        } else {
+            response = "Args presented: help, "+args[1]+", and others. help needs the args 'help', and the type of help you need. Please retry.";
+        }
+    } else {
+        response = "Unknown args presented: "+args+", please try again.";
+    }
+
+}
