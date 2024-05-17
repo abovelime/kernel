32bf83073dd14c3008bd97fade8df3949f340eb1
Liam B <143614295+abovelime@users.noreply.github.com>
2024-05-17 21:53:40 +1000

    this is the worst code i have ever produced

--- scripts/installOS/bootLoaderInstall.js
+++ scripts/installOS/bootLoaderInstall.js
@@ -6,11 +6,16 @@
 let osInstalled = false;
 let loadingAlert = 1;
 let preCheckAlert = 1;
+let welcomeAlert = 1;
 
 const onBoot = document.getElementById("on-boot");
 
 
 function onLoad() {
+
+    const loadhr = document.createElement('p');
+    const loadhrNode = document.createTextNode("===============")
+    loadhr.appendChild(loadhrNode);
 
     console.log("Loading Process Beginning...");
     const loadingP1 = document.createElement("p");
@@ -21,7 +26,7 @@ function onLoad() {
 
     onBoot.appendChild(loadingP1);
 
-
+    onBoot.appendChild(loadhr);
 
     console.log("Pre-check Starting");
     const preCheckP1 = document.createElement("p");
@@ -36,14 +41,54 @@ function onLoad() {
 
     if (osInstalled === true) {
         // send to os
+
+        // do later you moron there's no os rn
     } else {
-        // keep here
+
+        const noOSP = document.createElement("p");
+        const noOSNode = document.createTextNode("Booting into installOS config... [OK]");
+        noOSP.appendChild(noOSNode);
+        noOSP.setAttribute('id', 'noOSAlert');
+
+        onBoot.appendChild(noOSP);
+
+        // welcome node
+
+        const welcomeP1 = document.createElement("p");
+        const welcomeNode1 = document.createTextNode("Welcome to installOS!");
+        welcomeP1.appendChild(welcomeNode1);
+        welcomeP1.setAttribute('id', 'welcomeAlert'+welcomeAlert);
+        welcomeAlert++;
+
+        onBoot.appendChild(welcomeP1);
+
+        // help node
+
+        const welcomeP2 = document.createElement("p");
+        const welcomeNode2 = document.createTextNode("Type: 'help installos' for Help!");
+        welcomeP2.appendChild(welcomeNode2);
+        welcomeP2.setAttribute('id', 'welcomeAlert'+welcomeAlert);
+        welcomeAlert++;
+
+        onBoot.appendChild(welcomeP2);
+
+
+        const welcomehr = document.createElement('p');
+        const welcomehrnode = document.createTextNode("===============")
+        welcomehr.appendChild(welcomehrnode);
+
+        onBoot.appendChild(welcomehr);
+
     }
 
 }
 
 
 function preCheck() {
+
+    const prehr = document.createElement('p');
+    const prehrNode = document.createTextNode("===============")
+    prehr.appendChild(prehrNode);
 
     // init errors and info
     const preCheckP2 = document.createElement("p");
@@ -62,6 +107,10 @@ function preCheck() {
         preCheckNode2 = document.createTextNode("User doesn't have an OS installed! [INFO]")
     }
 
+    //
+    // os checker here later pls
+    //
+
     // append finished node
     preCheckP2.appendChild(preCheckNode2);
     preCheckP2.setAttribute('id', 'preCheckAlert'+preCheckAlert);
@@ -78,5 +127,5 @@ function preCheck() {
 
     // appends it
     onBoot.appendChild(preCheckP3);
-
+    onBoot.appendChild(prehr);
 }
